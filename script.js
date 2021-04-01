4e9783e7f377a0b4c7ac31c386702b3ccc25ef30
Black_Eyes <valentin@girod.fr>
2021-04-01 10:34:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $( document ).ready(function() {
     let client_id = '4fd4c2be1ee84fa2b3fe6683510614ab';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https://guarmanda.github.io'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
